1de719d0fad4f5390be346303b4d62a48f8a3058
Luis <luissoto2094@gmail.com>
2026-02-03 14:55:19 +0100

    feat: add DVELOPMENT GUIDELINE, add workflow, add example tests, ad coverage script

---
+++ src/math/math.c
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: GPL-2.0
+
+/**
+ * @file math.c
+ * @brief Basic math utility functions implementation
+ */
+
+#include "math.h"
+
+int math_add(int a, int b)
+{
+	return a + b;
+}
