4f2b2e3605ed993e4dca8364e3a56f0b208ff203
Luis Soto <107001111+LuisEd2094@users.noreply.github.com>
2026-02-22 14:22:52 +0100

    Add null check in ft_strncpy function
    Add null check for destination and source pointers in ft_strncpy.

--- src/kernel/wrappers/ft_strncpy.c
+++ src/kernel/wrappers/ft_strncpy.c
@@ -1,9 +1,12 @@
 #include <kernel/wrappers/helper.h>
+#include <stdint.h>
 
 char	*ft_strncpy(char *dst, const char *src, unsigned int len)
 {
 	unsigned int    i;
 
+	if (!dst || !src)
+		return NULL;	
 	i = 0;
 	while (src[i] && i < len)
 	{
