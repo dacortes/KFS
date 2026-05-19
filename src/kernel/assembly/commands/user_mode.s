f94c31e6c9ad6332741bd2f28e376406bd241c9f
dacortes <87203430+dacortes@users.noreply.github.com>
2026-05-19 11:39:50 +0200

    Load user data segments before iret to ring3 and remove stale return symbol

--- src/kernel/assembly/commands/user_mode.s
+++ src/kernel/assembly/commands/user_mode.s
@@ -1,13 +1,11 @@
 global switch_to_user_mode
 global get_current_privilege_level
-global kernel_return_eip
 global kernel_return_esp
 
 section .text
 [BITS 32]
 
 section .bss
-kernel_return_eip resd 1
 kernel_return_esp resd 1
 
 section .text
@@ -18,8 +16,6 @@ switch_to_user_mode:
 
 	mov eax, [ebp + 8]  ; user code entry point (in eax)
 	mov edx, [ebp + 12] ; user stack pointer (in edx)
-	mov ecx, [ebp + 4]  ; kernel return address after switch_to_user_mode
-	mov [kernel_return_eip], ecx
 	mov [kernel_return_esp], esp
 
 	mov ecx, cs
@@ -29,6 +25,14 @@ switch_to_user_mode:
 
 	cli
 
+
+	; Load ring-3 data selectors before entering user code.
+	; iret changes CS/SS/ESP/EFLAGS, but DS/ES/FS/GS keep previous values.
+	mov cx, 0x2B
+	mov ds, cx
+	mov es, cx
+	mov fs, cx
+	mov gs, cx
 
 	; Push user mode context for iret:
 	; SS (user data segment), ESP, EFLAGS, CS (user code segment), EIP
